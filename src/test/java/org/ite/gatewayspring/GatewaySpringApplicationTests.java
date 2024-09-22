8e92c18caded2bf3b6ee124c6f3df26055457b7c
MengseuThoeng <mengseu2004@gmail.com>
2024-09-22 17:35:52 +0700

    first commit

---
+++ src/test/java/org/ite/gatewayspring/GatewaySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ite.gatewayspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
